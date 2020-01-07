69e1ebc2a184539881cfb3c69328663bd2f9d98d
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-07 23:46:33 +0000

    symbols added

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed:  ${actual}   ===  ${expected}`);
+    console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !==  ${expected}`);
+    console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
   }
 
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "user");
-assertEqual(1, "1");
+assertEqual(1, 1);
