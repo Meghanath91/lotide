6324e5685a16daccd7cd73ae559fd4c54d7de6c7
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-08 23:38:39 +0000

    final code ready to push

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,21 +8,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(objects,values) {
-
-for (var item in objects) {
-  if(objects[item]=== values){
-    return(item);
+  for (let item in objects) {
+    if (objects[item] === values) {
+      return (item);
+    }
   }
-}
-}
+};
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 
 };
-findKeyByValue(bestTVShowsByGenre, "The Wire");
-
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
