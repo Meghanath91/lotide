3711708646f27315d952be1884c88fb8d9e6032c
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-08 22:49:38 +0000

    code linted

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(sentence) {
+  let result = {};
+  for (let i = 0;i<sentence.length;i++) {
+    if(sentence[i]!==" "){
+      if(result[sentence[i]]){
+        result[sentence[i]].push(i);
+      }
+        else{
+          result[sentence[i]]=[];
+          result[sentence[i]].push(i);
+        }
+
+      }
+  }
+  
+  console.log(result);
+  return result;
+};
+letterPositions("light house");
