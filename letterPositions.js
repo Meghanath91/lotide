ac6562adf58fe0c1feb63ebc222eb9b3502cb3d2
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-08 23:04:18 +0000

    code test added

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,24 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)===true) {
+    console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
+  } else if (eqArrays(actual,expected)=== false){
+    console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
+   
+  }
+}
+
 const letterPositions = function(sentence) {
   let result = {};
   for (let i = 0;i<sentence.length;i++) {
@@ -16,4 +37,8 @@ const letterPositions = function(sentence) {
   console.log(result);
   return result;
 };
-letterPositions("light house");
+const result1 = letterPositions("hello");
+assertArrayEqual(result1["h"],[ 0 ]);
+assertArrayEqual(result1["e"],[ 1]);
+assertArrayEqual(result1["l"],[ 2, 3 ]);
+assertArrayEqual(result1["o"],[ 4])
