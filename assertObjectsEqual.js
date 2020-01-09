5896ee00203c5063dd248faa1ca6c00a6df37876
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-09 06:03:16 +0000

    final refactored code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -12,16 +12,15 @@ const eqArrays = function(arryA, arryB) {
 };
 
 const eqObjects = function(objA,objB) {
-  let arrA = Object.keys(objA) ;
-  let arrB = Object.keys(objB) ;
-  if(arrA.length !== arrB.length) {
+  let arrA = Object.keys(objA);
+  let arrB = Object.keys(objB);
+  if (arrA.length !== arrB.length) {
     return false;
-  }
-   else {
-    for (let item of arrA){
+  } else {
+    for (let item of arrA) {
       
       if (Array.isArray(objA[item]) && Array.isArray(objB[item])) {
-        return (eqArrays(objA[item],objB[item]))
+        return (eqArrays(objA[item],objB[item]));
       } else if (objA[item] !== objB[item]) {
         console.log(objA[item], objB[item]);
         return false;
@@ -33,11 +32,11 @@ const eqObjects = function(objA,objB) {
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-    if (eqObjects(actual,expected)===true) {
-      console.log(`✅Assertion Passed:  ${inspect(actual)}   ===  ${inspect(expected)}`);
-    } else if (eqObjects(actual,expected)=== false){
-      console.log(`🛑Assertion Failed: ${inspect(actual)} !==  ${inspect(expected)}`);
+  if (eqObjects(actual,expected) === true) {
+    console.log(`✅Assertion Passed:  ${inspect(actual)}   ===  ${inspect(expected)}`);
+  } else if (eqObjects(actual,expected) === false) {
+    console.log(`🛑Assertion Failed: ${inspect(actual)} !==  ${inspect(expected)}`);
      
-    }
   }
-  assertObjectsEqual({p:3,q:2},{p:3,q:2})
+};
+assertObjectsEqual({p:3,q:2},{p:3,q:2});
