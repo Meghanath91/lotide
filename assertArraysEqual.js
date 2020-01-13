1220ea7da26013440aa67ed0ab7c8c575b5d7ad9
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-13 19:16:47 +0000

    assertArraysEqual refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 const assertArrayEqual = function(actual, expected) {
   if (eqArrays(actual,expected)===true) {
     console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
@@ -17,5 +6,5 @@ const assertArrayEqual = function(actual, expected) {
     console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
    
   }
-}
-assertArrayEqual([1,2,3],[1,2,3]);
+};
+module.exports = assertArrayEqual;
