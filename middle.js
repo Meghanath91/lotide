470185aaac1e5a5a84e7fea46f28b834095f0f15
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-13 15:01:29 +0000

    middle.js eslinted

--- middle.js
+++ middle.js
@@ -11,33 +11,31 @@ const eqArrays = function(actual, expected) {
   }
 };
 const assertArrayEqual = function(actual, expected) {
-  if (eqArrays(actual,expected)===true) {
+  if (eqArrays(actual,expected) === true) {
     console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
-  } else if (eqArrays(actual,expected)=== false){
+  } else if (eqArrays(actual,expected) === false) {
     console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
    
   }
-}
+};
 const middle = function(array) {
  
-  if(array.length === 1 || array.length === 2 ) {
+  if (array.length === 1 || array.length === 2) {
     return [];
-  }
-  else if(array.length%2===0) {
-    let middleIndex = array.length/2;
-    let middle = [array[middleIndex-1],array[middleIndex]] ;
+  } else if (array.length % 2 === 0) {
+    let middleIndex = array.length / 2;
+    let middle = [array[middleIndex - 1],array[middleIndex]];
     console.log(middle);
     return middle;
-  }
-  else if(array.length %2===1) {
-    middleIndex = Math.floor(array.length/2);
+  } else if (array.length % 2 === 1) {
+    let middleIndex = Math.floor(array.length / 2);
     let middle = [array[middleIndex]];
     console.log(middle);
     return middle;
   }
   
 };
-assertArrayEqual( middle([1,2,3,4]),[2,3]);
+assertArrayEqual(middle([1,2,3,4]),[2,3]);
 assertArrayEqual(middle([1,2,3,4,5]),[3]);
 assertArrayEqual(middle([1,2]),[]);
 assertArrayEqual(middle([3]),[]);
