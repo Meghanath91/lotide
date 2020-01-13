e4cdb24f95cdb837c07175e049756a71cbd10513
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-13 18:05:08 +0000

    headTest,assertEqualTest created

---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1,2,3,4]),1);
