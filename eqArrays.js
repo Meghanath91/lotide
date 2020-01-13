306994b876a5f9154faf15ea6ed11935d21c0161
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-13 18:54:21 +0000

    eqarray refactored using exports

--- eqArrays.js
+++ eqArrays.js
@@ -11,14 +11,4 @@ const eqArrays = function(actual, expected) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3 ]),false);
+module.exports = eqArrays;
