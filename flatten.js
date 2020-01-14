bbe7a01240d0513b23578948ecd14e6513c3fa57
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-14 00:40:50 +0000

    flatten code after eslint

--- flatten.js
+++ flatten.js
@@ -1,30 +1,30 @@
-// const eqArrays = function(actual, expected) {
-//   if (actual.length !== expected.length) {
-//     return false;
-//   } else {
-//     for (let i = 0; i < actual.length; i++) {
-//       if (actual[i] !== expected[i]) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   }
-// };
-// const assertArrayEqual = function(actual, expected) {
-//   if (eqArrays(actual,expected)===true) {
-//     console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
-//   } else if (eqArrays(actual,expected)=== false){
-//     console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual,expected) === true) {
+    console.log(`✅Assertion Passed:  ${actual}   ===  ${expected}`);
+  } else if (eqArrays(actual,expected) === false) {
+    console.log(`🛑Assertion Failed: ${actual} !==  ${expected}`);
    
-//   }
-// };
+  }
+};
 
-const flatten = function (array) {
-  let result=[];
+const flatten = function(array) {
+  let result = [];
 
-  for(item of array){
-    if(Array.isArray(item)){
-      for(element of item) {
+  for (item of array) {
+    if (Array.isArray(item)) {
+      for (element of item) {
         result.push(element);
 
       }
@@ -37,4 +37,4 @@ const flatten = function (array) {
 
 };
 
-console.log(flatten([1,2,3,[4,5]]));
+assertArrayEqual((flatten([1,2,3,[4,5]])),[1,2,3,4,5]);
